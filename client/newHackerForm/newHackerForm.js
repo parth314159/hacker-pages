269d3bd51dacdc5b10d6d4d909ec7531b4e12633
Parth Shah <shahparth8891@gmail.com>
2016-08-08 20:10:09 -0400

    added

--- client/newHackerForm/newHackerForm.js
+++ client/newHackerForm/newHackerForm.js
@@ -15,7 +15,7 @@ Template.newHackerForm.events({
       allegiance: allegiance
     }
 
-    Hackers.insert(newHacker);
+    Meteor.call('addhacker',newHacker);
     alert("Insert was successful");
   }
 
